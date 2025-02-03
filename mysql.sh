05bb4ec1ce48efe8e367365c7a4fac0d1b33873e
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-03 15:20:47 +0530

    expense-project using  shellscript added

--- mysql.sh
+++ mysql.sh
@@ -4,17 +4,23 @@
     G="\e[32m"
     Y="\e[33m"
     N="\e[0m"
+
     Log_folder="/var/log/expense-logs"
-    logfile=$(echo $0 | cut -d "." -f 1)
+
+    [ ! -d "$Log_folder" ] && mkdir -p "$Log_folder"
+    logfile=$(basename $0 | cut -d "." -f 1)
+
     timestamp=$(date '+%d-%m-%Y-%H-%M-%S')
+
     logfilename="$Log_folder/$logfile-$timestamp.log"
     validate(){
         if [ $1 -eq 0 ];
         then
             echo -e "$2 ...$G successfully $N"
-            exit 1
         else
             echo -e "Error:: $2  .....$R failed $N"
+            exit 1
+
         fi
     }
     CHECKROOT(){
