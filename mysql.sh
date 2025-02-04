d4a180f6cb2fcc211ce775c3e567ce43397b9cd7
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-04 13:02:55 +0530

    expense-project using  shellscript added

--- mysql.sh
+++ mysql.sh
@@ -1,48 +1,58 @@
-    #!/bin/bash
-    userid=$(id -u)
-    R="\e[31m"
-    G="\e[32m"
-    Y="\e[33m"
-    N="\e[0m"
+#!/bin/bash
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-    Log_folder="/var/log/expense-logs"
+Log_folder="/var/log/expense-logs"
 
-    [ ! -d "$Log_folder" ] && mkdir -p "$Log_folder"
-    logfile=$(basename $0 | cut -d "." -f 1)
+[ ! -d "$Log_folder" ] && mkdir -p "$Log_folder"
 
-    timestamp=$(date '+%d-%m-%Y-%H-%M-%S')
+logfile=$(basename $0 | cut -d "." -f 1)
 
-    logfilename="$Log_folder/$logfile-$timestamp.log"
-    validate(){
-        if [ $1 -eq 0 ];
-        then
-            echo -e "$2 ...$G successfully $N"
-        else
-            echo -e "Error:: $2  .....$R failed $N"
-            exit 1
+timestamp=$(date '+%d-%m-%Y-%H-%M-%S')
 
-        fi
-    }
-    CHECKROOT(){
-        if [ $userid -ne 0 ];
-        then
-        echo "Error:: you must have sudo access to execute this script"
+logfilename="$Log_folder/$logfile-$timestamp.log"
+validate(){
+    if [ $1 -eq 0 ];
+    then
+        echo -e "$2 ...$G successfully $N"
+    else
+        echo -e "Error:: $2  .....$R failed $N"
         exit 1
-        fi
-    }
-    echo "script started executing at $timestamp" &>>$logfilename
 
-    CHECKROOT
+    fi
+}
+CHECKROOT(){
+    if [ $userid -ne 0 ];
+    then
+    echo "Error:: you must have sudo access to execute this script"
+    exit 1
+    fi
+}
+echo "script started executing at $timestamp" &>>$logfilename
 
-    dnf install mysql-server -y &>>$logfilename
-    validate $? "installing mysql server"
+CHECKROOT
 
-    systemctl enable mysqld &>>$logfilename
-    validate $? "enabling mysql server"
+dnf install mysql-server -y &>>$logfilename
+validate $? "installing mysql server"
 
-    systemctl start mysqld &>>$logfilename
-    validate $? "starting mysql server"
+systemctl enable mysqld &>>$logfilename
+validate $? "enabling mysql server"
 
-    mysql_secure_installation --set-root-pass ExpenseApp@1
+systemctl start mysqld &>>$logfilename
+validate $? "starting mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+validate $? "setting root password"
+
+mysql -h mysql.lokeshportfo.site -u root -pExpenseApp@1 -e "show databases;" &>>$logfilename
+
+if [ $? -ne 0 ];then
+    echo "ERROR:: mysql root password not setup properly" &>>$logfilename
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfilename
     validate $? "setting root password"
-
+else
+    echo -e "mysql root password is already setup done..... $Y INSTALLED $N"
+fi
